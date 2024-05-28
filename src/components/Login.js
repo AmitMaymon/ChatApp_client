84d4b9a4518157010b3d4ded837e09477614cb1f
amit <amit.maymon00@gmail.com>
2024-05-28 21:01:50 +0300

    Fixed url

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ function Login(props) {
     useEffect(() => {
         const token = localStorage.getItem('token')
         if (token && !props.loggedOut) {
-            const newSocket = io('https://chatapp-server-x9b6.onrender.com:10000', {
+            const newSocket = io('https://chatapp-server-x9b6.onrender.com', {
                 transports: ['websocket'],
                 query: {
                     token: token
@@ -47,7 +47,7 @@ function Login(props) {
             return
         }
 
-        const newSocket = io('https://chatapp-server-x9b6.onrender.com:10000', {
+        const newSocket = io('https://chatapp-server-x9b6.onrender.com', {
             transports: ['websocket'],
             query: {
                 username: username,
@@ -96,7 +96,7 @@ function Login(props) {
             setConfirmPasswordErrors(validationResult.getErrors().confirmPassword)
             return
         }
-        const newSocket = io('https://chatapp-server-x9b6.onrender.com:10000', {
+        const newSocket = io('https://chatapp-server-x9b6.onrender.com', {
             transports: ['websocket'],
             query: {
                 username: username,
